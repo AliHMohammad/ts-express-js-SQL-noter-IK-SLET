b5c9d9e22fab8e07164b03aa50940d291ae0dd54
AliHMohammad <alihmohammad@hotmail.com>
2023-10-17 22:06:02 +0200

    added comments

--- src/Database/data-source.ts
+++ src/Database/data-source.ts
@@ -1,10 +1,12 @@
-import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { Artists } from "../Model/Artists.js";
 import { Albums } from "../Model/Albums.js";
 import { Tracks } from "../Model/Tracks.js";
-import { config } from "dotenv";
 
+import "reflect-metadata";
+//Importer "reflect-metadata"
+import { config } from "dotenv";
+//Kør config(), så den kan læse din env fil. Config importeres ^
 config();
 
 export const AppDataSource = new DataSource({
@@ -13,8 +15,11 @@ export const AppDataSource = new DataSource({
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    port: 3306,
+    port: 3306, //PORT PÅ DATABASEN!!!!!
     synchronize: true,
     logging: true,
-    entities: [Artists, Albums, Tracks],
+    entities: [Artists, Albums, Tracks], //Dine modeller, som du har auto-genereret.
 });
+
+
+
