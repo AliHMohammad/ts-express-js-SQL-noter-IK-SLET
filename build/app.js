b186115229379de87a467637d071217542c61387
AliHMohammad <alihmohammad@hotmail.com>
2023-11-12 11:01:12 +0100

    refactored app

---
+++ build/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import { artistRouter } from "./Route/artist-router.js";
+import { albumRouter } from "./Route/album-route.js";
+import { trackRouter } from "./Route/track-route.js";
+import { otherRouter } from "./Route/other-route.js";
+export default function createServer() {
+    const app = express();
+    app.use(express.json());
+    app.use(cors());
+    app.use("/", artistRouter, albumRouter, trackRouter, otherRouter);
+    return app;
+}
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,8 @@
-import express, { Express, Request, Response } from "express";
-import cors from "cors";
-import { artistRouter } from "./Route/artist-router.js";
-import { albumRouter } from "./Route/album-route.js";
-import { trackRouter } from "./Route/track-route.js";
-import { otherRouter } from "./Route/other-route.js";
 import prisma from "./Database/data-source.js";
+import createServer from "./app.js";
 
-
-const app: Express = express();
 const port = 3000;
-
-app.use(express.json());
-app.use(cors());
-
-app.get("/", (request: Request, response: Response) => {
-    response.send("Server is running");
-});
-
-app.use("/", artistRouter, albumRouter, trackRouter, otherRouter);
+const app = createServer()
 
 app.listen(port, async () => {
     await prisma.$connect();
