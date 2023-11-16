18dde8948b494feba4a1f9e4d3efc9a77afa7b6f
AliHMohammad <alihmohammad@hotmail.com>
2023-11-16 09:03:21 +0100

    added logs to prisma

--- src/Database/data-source.ts
+++ src/Database/data-source.ts
@@ -5,7 +5,9 @@ config();
 
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+});
 
 
 export default prisma
